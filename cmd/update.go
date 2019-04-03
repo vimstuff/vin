6d4237870ea68e8be4c94d01a2ff1901ff6ec95f
Matthew Rankin <matthew@cumulusware.com>
2019-04-03 09:40:39 -0500

    First pass at update command. Dirs are hardcoded (bad).

--- cmd/update.go
+++ cmd/update.go
@@ -6,8 +6,13 @@
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"os/exec"
+	"path"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 )
 
@@ -19,8 +24,46 @@ var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update Vim dependencies",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Update Janus
+		fmt.Println("Updating Janus (running rake)")
+		janusDir, err := homedir.Expand("~/.vim")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = rake(janusDir)
+		if err != nil {
+			log.Fatalf("rake command to update Janus failed: %s", err)
+		}
 
-		// FIXME(mdr): Update.
-		log.Println("FIXME: Should update here.")
+		// Determine the custom Janus plug-ins installed and update.
+		pluginDir, err := homedir.Expand("~/.janus")
+		if err != nil {
+			log.Fatal(err)
+		}
+		plugins, err := ioutil.ReadDir(pluginDir)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, plugin := range plugins {
+			fmt.Println(plugin.Name())
+			p := path.Join(pluginDir, plugin.Name())
+			err = gitPull(p)
+			if err != nil {
+				log.Fatalf("cmd.Run() failed with %s\n", err)
+			}
+		}
 	},
+}
+
+func rake(dir string) error {
+	// Update Janus
+	c := exec.Command("rake")
+	c.Dir = dir
+	return c.Run()
+}
+
+func gitPull(dir string) error {
+	c := exec.Command("git", "pull")
+	c.Dir = dir
+	return c.Run()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/vimstuff/vin
 go 1.12
 
 require (
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
 )
