0db69eca1a0a20277d7ee976a3939bbf6a4692b7
Matthew Rankin <matthew@cumulusware.com>
2019-04-03 19:26:35 -0500

    List included Janus plug-ins that are disabled.

--- cmd/list.go
+++ cmd/list.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"regexp"
 
 	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
@@ -20,10 +21,24 @@ func init() {
 
 var listCmd = &cobra.Command{
 	Use:     "list",
-	Aliases: []string{"ls"},
-	Short:   "list Vim plug-ins",
+	Aliases: []string{"ls", "l"},
+	Short:   "List Vim plug-ins",
 	Run: func(cmd *cobra.Command, args []string) {
-		// FIXME(mdr): List the disabled Janus plugins.
+		// Disabled Janus included plug-ins.
+		vimrcBefore, err := homedir.Expand("~/.vimrc.before")
+		if err != nil {
+			log.Fatal(err)
+		}
+		disabled := findDisabled(vimrcBefore)
+		if len(disabled) > 0 {
+			fmt.Println("The following Janus included plug-ins are disabled:")
+			for _, d := range disabled {
+				fmt.Printf("  %s\n", d)
+			}
+		} else {
+			fmt.Println("None of the included Janus plug-ins are disabled.")
+			fmt.Println("")
+		}
 
 		// Determine the custom Janus plug-ins installed and update.
 		pluginDir, err := homedir.Expand("~/.janus")
@@ -34,8 +49,27 @@ var listCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal(err)
 		}
-		for _, plugin := range plugins {
-			fmt.Println(plugin.Name())
+		if len(plugins) > 0 {
+			fmt.Println("The following additional plug-ins are installed:")
+			for _, plugin := range plugins {
+				fmt.Printf("  %s\n", plugin.Name())
+			}
+		} else {
+			fmt.Println("There are no additional plug-ins installed.")
 		}
 	},
+}
+
+func findDisabled(file string) []string {
+	submatches := []string{}
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		return submatches
+	}
+	re := regexp.MustCompile(`[^#][^ ]call janus#disable_plugin\('(.*)'\)`)
+	matches := re.FindAllSubmatch(b, -1)
+	for _, match := range matches {
+		submatches = append(submatches, string(match[1]))
+	}
+	return submatches
 }
